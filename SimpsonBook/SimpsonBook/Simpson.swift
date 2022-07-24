25675f12cf5c3099044a02c48062c90f57a67eaa
oguzhansever <103364929+oguzhansever@users.noreply.github.com>
2022-07-24 19:39:23 +0300

    Add files via upload

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,28 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Oğuzhan Sever on 24.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName:String,simpsonJob:String,simpsonImage:UIImage) {
+        
+    name = simpsonName
+    job = simpsonJob
+    image = simpsonImage
+    
+}
+    
+}
+    
+    
+    
